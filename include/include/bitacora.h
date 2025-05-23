c46bb7efbd7b8732f6f2abdb8716ebfea9bd2ef7
Dsanabria02 <100260303+Dsanabria02@users.noreply.github.com>
2025-05-22 23:02:57 -0600

    Update bitacora.h

--- include/include/bitacora.h
+++ include/include/bitacora.h
@@ -1,1 +1,12 @@
+#ifndef BITACORA_H
+#define BITACORA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+void registrar_evento(const char *accion, pid_t pid, int inicio, int fin);
+
+#endif
 
