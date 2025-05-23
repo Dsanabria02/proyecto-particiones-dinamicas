40aca8d541883f99ad3d35bf0afd2773b83c609c
Dsanabria02 <100260303+Dsanabria02@users.noreply.github.com>
2025-05-22 23:08:10 -0600

    Update finalizador.c

--- src/finalizador.c
+++ src/finalizador.c
@@ -1,1 +1,21 @@
+#include "../include/compartido.h"
+#include "../include/bitacora.h"
+
+int main() {
+    int shmid = shmget(CLAVE_MEMORIA, 0, 0666);
+    int semid = semget(CLAVE_SEMAFORO, 1, 0666);
+
+    if (shmid != -1) {
+        shmctl(shmid, IPC_RMID, NULL);
+        printf("Memoria compartida eliminada.\n");
+    }
+
+    if (semid != -1) {
+        semctl(semid, 0, IPC_RMID);
+        printf("Semáforo eliminado.\n");
+    }
+
+    registrar_evento("Simulación finalizada", getpid(), 0, 0);
+    return 0;
+}
 
