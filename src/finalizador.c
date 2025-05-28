58e03d945cddfe7ac63789ddaa20c6214f13f480
Dsanabria02 <100260303+Dsanabria02@users.noreply.github.com>
2025-05-27 21:15:27 -0600

    Update finalizador.c

--- src/finalizador.c
+++ src/finalizador.c
@@ -15,6 +15,12 @@ int main() {
         printf("Semáforo eliminado.\n");
     }
 
+    // Eliminación extra de la memoria con clave 0x1235
+    int extra_shmid = shmget(0x1235, 1, 0666);
+    if (extra_shmid != -1) {
+        shmctl(extra_shmid, IPC_RMID, NULL);
+    }
+
     registrar_evento("Simulación finalizada", getpid(), 0, 0);
     return 0;
 }
