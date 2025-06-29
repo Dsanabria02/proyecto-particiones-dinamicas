4124857421f5ada4fda4184d7937570cd298e801
Dsanabria02 <100260303+Dsanabria02@users.noreply.github.com>
2025-06-28 23:06:57 -0600

    Update User.java

--- Proyecto-3-FS-Drive/Proyecto 3/src/main/java/server/users/User.java
+++ Proyecto-3-FS-Drive/Proyecto 3/src/main/java/server/users/User.java
@@ -4,11 +4,26 @@ import server.fs.DirectoryNode;
 
 public class User {
     private String username;
+    private String password;
     private int maxSize;
     private DirectoryNode root;
     private DirectoryNode shared;
     private DirectoryNode currentDirectory;
 
+    // Constructor vacío para que Gson pueda deserializar
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.maxSize = 10240;
+        this.root = new DirectoryNode("root");
+        this.shared = new DirectoryNode("shared");
+        this.currentDirectory = root;
+    }
+
+    // Constructor adicional con maxSize como en la versión de tu compañera
     public User(String username, int maxSize) {
         this.username = username;
         this.maxSize = maxSize;
@@ -21,19 +36,53 @@ public class User {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public DirectoryNode getRootDirectory() {
+        return root;
+    }
+
+    // Método adicional para obtener root como lo nombró tu compañera
     public DirectoryNode getRoot() {
         return root;
+    }
+
+    public void setRootDirectory(DirectoryNode root) {
+        this.root = root;
+    }
+
+    public DirectoryNode getShared() {
+        return shared;
+    }
+
+    public void setShared(DirectoryNode shared) {
+        this.shared = shared;
     }
 
     public DirectoryNode getCurrentDirectory() {
         return currentDirectory;
     }
 
-    public void setCurrentDirectory(DirectoryNode dir) {
-        this.currentDirectory = dir;
+    public void setCurrentDirectory(DirectoryNode currentDirectory) {
+        this.currentDirectory = currentDirectory;
     }
 
-    public DirectoryNode getShared() {
-        return shared;
-    }
 }
