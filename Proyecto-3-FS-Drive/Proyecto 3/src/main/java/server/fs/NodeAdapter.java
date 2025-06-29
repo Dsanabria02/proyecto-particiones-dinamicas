7c5f1b7a9b1cd7d5c3ad3848a407c24e56c16b1f
Dsanabria02 <100260303+Dsanabria02@users.noreply.github.com>
2025-06-29 05:09:38 -0600

    Update NodeAdapter.java

--- Proyecto-3-FS-Drive/Proyecto 3/src/main/java/server/fs/NodeAdapter.java
+++ Proyecto-3-FS-Drive/Proyecto 3/src/main/java/server/fs/NodeAdapter.java
@@ -1,28 +1,50 @@
 package server.fs;
 
 import com.google.gson.*;
+
 import java.lang.reflect.Type;
+import java.util.List;
 
 public class NodeAdapter implements JsonSerializer<Node>, JsonDeserializer<Node> {
 
     @Override
-    public JsonElement serialize(Node src, Type typeOfSrc, JsonSerializationContext context) {
-        JsonObject obj = context.serialize(src).getAsJsonObject();
-        obj.addProperty("type", src instanceof DirectoryNode ? "directory" : "file");
-        return obj;
+    public JsonElement serialize(Node node, Type typeOfSrc, JsonSerializationContext context) {
+        JsonObject json = new JsonObject();
+
+        json.addProperty("name", node.getName());
+        json.addProperty("created", node.getCreated().toString());
+        json.addProperty("modified", node.getModified().toString());
+        json.addProperty("size", node.getSize());
+
+        if (node instanceof FileNode fileNode) {
+            json.addProperty("type", "file");
+            json.addProperty("extension", fileNode.getExtension());
+            json.addProperty("content", fileNode.getContent());
+        } else if (node instanceof DirectoryNode dirNode) {
+            json.addProperty("type", "directory");
+
+            // Serializar los hijos (evitando ciclos)
+            JsonArray children = new JsonArray();
+            for (Node child : dirNode.getChildren()) {
+                children.add(serialize(child, child.getClass(), context));
+            }
+            json.add("children", children);
+        }
+
+        return json;
     }
 
     @Override
-    public Node deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+    public Node deserialize(JsonElement jsonElement, Type typeOfT, JsonDeserializationContext context)
             throws JsonParseException {
-        JsonObject obj = json.getAsJsonObject();
+        JsonObject obj = jsonElement.getAsJsonObject();
         String type = obj.get("type").getAsString();
 
         switch (type) {
             case "directory":
-                return context.deserialize(json, DirectoryNode.class);
+                return context.deserialize(jsonElement, DirectoryNode.class);
             case "file":
-                return context.deserialize(json, FileNode.class);
+                return context.deserialize(jsonElement, FileNode.class);
             default:
                 throw new JsonParseException("Tipo desconocido: " + type);
         }
